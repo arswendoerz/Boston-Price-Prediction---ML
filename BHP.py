f4cd6a5405547414d32a786be7cc5be72b587da3
arswendoerz <144572709+arswendoerz@users.noreply.github.com>
2024-06-19 22:55:01 +0700

    Add files via upload

--- BHP.py
+++ BHP.py
@@ -1,77 +1,180 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sklearn.datasets
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn import metrics
 from sklearn.datasets import fetch_openml
 
-# Mengambil dataset Boston dari openml
-boston = fetch_openml(name='boston', version=1, as_frame=True)
-house_price_dataframe = boston.frame
 
-# Menampilkan beberapa baris pertama dari dataset
-print(house_price_dataframe.head())
+# In[2]:
 
-# Menambahkan kolom 'price' ke dataframe
-house_price_dataframe['price'] = house_price_dataframe['target']
 
-# Menampilkan bentuk dan informasi dari dataset
-print(house_price_dataframe.shape)
-print(house_price_dataframe.isnull().sum())
-print(house_price_dataframe.describe())
+house_price_dataset = pd.read_csv("boston.csv")
 
-# Menghitung korelasi
+
+# In[4]:
+
+
+house_price_dataset
+
+
+# In[73]:
+
+
+print(house_price_dataset)
+
+
+# In[74]:
+
+
+house_price_dataframe = pd.DataFrame(boston.data, columns=boston.feature_names)
+
+
+# In[75]:
+
+
+house_price_dataframe.head()
+
+
+# In[77]:
+
+
+house_price_dataframe['price'] = boston.target
+
+
+# In[78]:
+
+
+house_price_dataframe.head()
+
+
+# In[79]:
+
+
+house_price_dataframe.shape
+
+
+# In[80]:
+
+
+house_price_dataframe.isnull().sum()
+
+
+# In[81]:
+
+
+house_price_dataframe.describe()
+
+
+# In[121]:
+
+
 correlation = house_price_dataframe.corr()
 
-# Visualisasi korelasi
+
+# In[83]:
+
+
 plt.figure(figsize=(10,10))
 sns.heatmap(correlation, cbar=True, square=True, fmt='.1f', annot=True, annot_kws={'size':8}, cmap='Blues')
 
-# Memisahkan fitur dan target
-X = house_price_dataframe.drop(['price', 'target'], axis=1)
+
+# In[120]:
+
+
+X = house_price_dataframe.drop(['price'], axis=1)
 Y = house_price_dataframe['price']
+
+
+# In[122]:
+
 
 print(X)
 print(Y)
 
-# Membagi data menjadi data latih dan data uji
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+
+# In[106]:
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 2)
+
+
+# In[117]:
+
 
 print(X.shape, X_train.shape, X_test.shape)
 
-# Membuat dan melatih model
+
+# In[134]:
+
+
 model = XGBRegressor()
+
+
+# In[135]:
+
+
 model.fit(X_train, Y_train)
 
-# Prediksi pada data latih
+
+# In[136]:
+
+
 training_data_prediction = model.predict(X_train)
+
+
+# In[137]:
+
+
 print(training_data_prediction)
 
-# Menghitung error pada data latih
+
+# In[138]:
+
+
+# R squared error
 score_1 = metrics.r2_score(Y_train, training_data_prediction)
+
+# Mean Absolute Error
 score_2 = metrics.mean_absolute_error(Y_train, training_data_prediction)
 
 print("R squared error : ", score_1)
 print('Mean Absolute Error : ', score_2)
 
-# Visualisasi hasil prediksi pada data latih
+
+# In[142]:
+
+
 plt.scatter(Y_train, training_data_prediction)
 plt.xlabel("Actual Prices")
 plt.ylabel("Predicted Prices")
 plt.title("Actual Price vs Predicted Price")
 plt.show()
 
-# Prediksi pada data uji
+
+# In[140]:
+
+
 test_data_prediction = model.predict(X_test)
 
-# Menghitung error pada data uji
+
+# In[141]:
+
+
 score_1 = metrics.r2_score(Y_test, test_data_prediction)
+
+# Mean Absolute Error
 score_2 = metrics.mean_absolute_error(Y_test, test_data_prediction)
 
 print("R squared error : ", score_1)
 print('Mean Absolute Error : ', score_2)
+
